74b2736d84d094b818a837a6f7f137df560444b9
Katarhi <mirrorwitch@yandex.ru>
2024-11-13 18:34:56 +0300

    update

---
+++ Лаба 5/FindWords.java
@@ -0,0 +1,30 @@
+import java.util.regex.*;
+
+public class FindWords {
+    public static void main(String[] args) {
+        try {
+            String text = "Emma E.A. watched THE sunseT fade into the horizon";
+
+            char begin = 'E';
+            String regex = String.format("(?i)\\b%c[a-z]+", begin);
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(text);
+            
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+        } catch (PatternSyntaxException e) {
+            System.out.println(e);
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+/*
+ * Необходимо написать программу, которая будет искать все слова в 
+заданном тексте, начинающиеся с заданной буквы, и выводить их на 
+экран. При этом программа должна использовать регулярные выражения 
+для поиска слов и обрабатывать возможные ошибки. 
+ */
---
+++ Лаба 5/Password.java
@@ -0,0 +1,36 @@
+import java.util.regex.*;
+
+public class Password {
+    public static void main(String[] args) {
+        String password = "iQwerty1";
+
+        try {
+            Matcher matcher = Pattern.compile(".{8,16}").matcher(password);
+            boolean inRange = matcher.find();
+            matcher = Pattern.compile("[A-Z]{1,}").matcher(password);
+            boolean upSim = matcher.find();
+            matcher = Pattern.compile("[a-z]{1,}").matcher(password);
+            boolean lowSim = matcher.find();
+            matcher = Pattern.compile("[0-9]{1,}").matcher(password);
+            boolean numMatch = matcher.find();
+            matcher = Pattern.compile("_?").matcher(password);
+            boolean specSim = matcher.find();
+
+            if (inRange && upSim && lowSim && numMatch && specSim)
+                System.out.println("Valid password");
+            else
+                System.out.println("Invalid password");
+            
+        } catch (PatternSyntaxException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+/*
+ * Необходимо написать программу, которая будет проверять корректность 
+ввода пароля. Пароль должен состоять из латинских букв и цифр, быть 
+длиной от 8 до 16 символов и содержать хотя бы одну заглавную букву и 
+одну цифру. При этом программа должна использовать регулярные 
+выражения для проверки пароля и обрабатывать возможные ошибки.
+ */
